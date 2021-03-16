71f59fb8070ad85b39d56c02baedbbd2c5c14503
Rottzll <ordenador.0930@gmail.com>
2021-03-16 11:40:51 +0900

    21.03.16

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <h1> start react </h1>
+      <p>CSS 적용</p>
+      </div>
+  );
+}
+
+export default App;
